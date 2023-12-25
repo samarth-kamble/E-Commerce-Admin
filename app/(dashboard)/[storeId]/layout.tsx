18bce7957298ae973cae8bfbc9035b1c5bb002d1
Samarth Kamble <kamble.samarth91@gmail.com>
2023-12-25 13:29:05 +0530

    Create a store added and the store switcher

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
@@ -24,7 +25,7 @@ export default async function DashboardLayout({
 
   return (
     <>
-      <div>This Will be a nav bar</div>
+      <Navbar />
       {children}
     </>
   );
---
+++ components/main-nav.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { usePathname, useParams } from "next/navigation";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+
+export default function MainNav({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) {
+  const pathname = usePathname();
+  const params = useParams();
+
+  const routes = [
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
+    },
+  ];
+  return (
+    <nav className={cn("flex items-center space-x-4 lg:space-x-6", className)}>
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </nav>
+  );
+}
